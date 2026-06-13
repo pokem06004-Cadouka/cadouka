4a16a2a29439bbbfa8fc70ed0502790f0d4718b1
Cadouka <pokem06004@gmail.com>
2026-06-13 17:47:45 +0800

    dSpeed up search alias bulk1 import14

--- image_utils.py
+++ image_utils.py
@@ -431,7 +431,7 @@ def generate_market_card_image(product, prices, selected_grade="PSA10", jpy_rate
     if product_image:
         product_box = create_contain_image(
             product_image,
-            (100, 200),
+            (600, 800),
             bg_color=(250, 250, 250)
         )
 
