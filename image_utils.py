fe8c1b36af89f5140ea20a2a9656bd92fd73d86e
Cadouka <pokem06004@gmail.com>
2026-06-14 23:37:08 +0800

    dSpeed up search alias bulk1 import14

--- image_utils.py
+++ image_utils.py
@@ -573,7 +573,7 @@ def generate_market_card_image(product, prices, selected_grade="PSA10", jpy_rate
     # =========================
     # 左側商品圖：維持目前圖片大小
     # =========================
-    left_box = (91, 143, 467, 655)
+    left_box = (91, 148, 467, 658)
 
     draw.rounded_rectangle(
         left_box,
