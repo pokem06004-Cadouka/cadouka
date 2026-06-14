6622365c72ba6e2c51dfdbced4ee909093e6a7ff
Cadouka <pokem06004@gmail.com>
2026-06-14 22:04:35 +0800

    dSpeed up search alias bulk1 import14

--- image_utils.py
+++ image_utils.py
@@ -558,7 +558,7 @@ def generate_market_card_image(product, prices, selected_grade="PSA10", jpy_rate
     stat_twd_font = get_font(35, bold=False)
 
     # 外層白色卡片：圓角更乾淨
-    outer_box = (8, 8, canvas_width - 8, canvas_height - 8)
+    outer_box = (8, 16 , canvas_width - 8, canvas_height)
     draw.rounded_rectangle(
         outer_box,
         radius=18,
